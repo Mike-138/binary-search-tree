bdeb33f25484619e3f24f2f8c6bea92980e5e2da
Michael <mikemcd.bbs@gmail.com>
2023-11-03 13:16:40 -0400

    Begin structuring binary tree

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -3,3 +3,18 @@ function Node(value) {
     this.right = null;
     this.left = null;
 }
+
+function Tree(array) {
+
+    const _buildTree = () => {
+        // TODO: IMPLEMENT
+    }
+
+    this.root = _buildTree();
+
+    return {
+        get root() {
+            return this.root
+        }
+    }
+}
