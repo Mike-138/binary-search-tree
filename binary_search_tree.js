28b01cf1d17669aef4fdc12de23e26efb363025b
Michael <mikemcd.bbs@gmail.com>
2023-11-09 19:57:13 -0500

    Lint program files

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -1,253 +1,250 @@
 function TreeNode(value) {
-    return {
-        value,
-        left: null,
-        right: null
-    }
+  return {
+    value,
+    left: null,
+    right: null,
+  };
 }
 
 function Tree(array) {
+  const _sortedArray = [...new Set(array)].sort((a, b) => a - b);
 
-    const _sortedArray = [...new Set(array)].sort((a, b) => a - b);
-
-    const _buildTree = (array, start = 0, end = array.length - 1) => {
-
-        if (start > end) {
-            return null;
-        }
-
-        const mid = Math.floor((start + end) / 2);
-        const root = TreeNode(array[mid]);
-
-        root.left = _buildTree(array, start, mid - 1);
-        root.right = _buildTree(array, mid + 1, end);
-
-        return root;
+  const _buildTree = (array, start = 0, end = array.length - 1) => {
+    if (start > end) {
+      return null;
     }
 
-    let root = _buildTree(_sortedArray);
+    const mid = Math.floor((start + end) / 2);
+    const root = TreeNode(array[mid]);
 
-    const prettyPrint = (node = root, prefix = "", isLeft = true) => {
-        if (node === null) {
-          return;
-        }
-        if (node.right !== null) {
-          prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-        }
-        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-        if (node.left !== null) {
-          prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-        }
-      };
+    root.left = _buildTree(array, start, mid - 1);
+    root.right = _buildTree(array, mid + 1, end);
 
-    const insertNode = (value) => {
-        if (!root) {
-            root = TreeNode(value);
-            return root;
-        }
+    return root;
+  };
 
-        let node = root;
-        let parentNode;
-        let leaf;
+  let root = _buildTree(_sortedArray);
 
-        while (node) {
-            parentNode = node;
-            if (value < node.value) {
-                node = node.left;
-                leaf = "left";
-            } else {
-                node = node.right;
-                leaf = "right";
-            }
-        }
+  const prettyPrint = (node = root, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
 
-        parentNode[leaf] = TreeNode(value);
-
-        return root;
-    };
-
-    const deleteNode = (value) => {
-        let node = root;
-        let parentNode;
-        let leaf;
-
-        while (node) {
-            if (value < node.value) {
-                parentNode = node;
-                node = node.left;
-                leaf = "left";
-            } else if (value > node.value) {
-                parentNode = node;
-                node = node.right;
-                leaf = "right";
-            // Execute once node has been found
-            } else {
-                // If node is a leaf node
-                if (!node.left && !node.right) {
-                    parentNode[leaf] = null;
-                    return root;
-                // If node has a left and right child
-                } else if (node.left && node.right) {
-                    let replacementNode = node.right;
-                    parentNode = node;
-                    leaf = "right";
-                    while (replacementNode.left) {
-                        parentNode = replacementNode;
-                        replacementNode = replacementNode.left;
-                        leaf = "left";
-                    }
-                    parentNode[leaf] = replacementNode.right;
-                    node.value = replacementNode.value;
-                    return root;
-                // If node only has one child
-                } else {
-                    parentNode[leaf] = (node.left) ? node.left : node.right;
-                    return root;
-                }
-            }
-        }
-        // Execute only if node does not exist in tree
-        return root;
-    };
-
-    const findNode = (value) => {
-        let node = root;
-
-        while (node) {
-            if (value < node.value) {
-                node = node.left;
-            } else if (value > node.value) {
-                node = node.right;
-            } else {
-                return node;
-            }
-        }
-        return null;
-    };
-
-    const levelOrder = (callback) => {
-        if (!root) {
-            return;
-        }
-
-        let array = [];
-        let node;
-        let queue = [root];
-        while (queue.length > 0) {
-            node = queue.shift();
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
-            (callback) ? array.push(callback(node.value)) : array.push(node.value);
-        }
-
-        return array;
+  const insertNode = (value) => {
+    if (!root) {
+      root = TreeNode(value);
+      return root;
     }
 
-    const inOrder = (callback, node = root, array = []) => {
-        if (!node) {
-            return;
-        }
+    let node = root;
+    let parentNode;
+    let leaf;
 
-        inOrder(callback, node.left, array);
-        (callback) ? array.push(callback(node.value)) : array.push(node.value);
-        inOrder(callback, node.right, array);
-
-        return array;
+    while (node) {
+      parentNode = node;
+      if (value < node.value) {
+        node = node.left;
+        leaf = "left";
+      } else {
+        node = node.right;
+        leaf = "right";
+      }
     }
 
-    const preOrder = (callback, node = root, array = []) => {
-        if (!node) {
-            return;
+    parentNode[leaf] = TreeNode(value);
+
+    return root;
+  };
+
+  const deleteNode = (value) => {
+    let node = root;
+    let parentNode;
+    let leaf;
+
+    while (node) {
+      if (value < node.value) {
+        parentNode = node;
+        node = node.left;
+        leaf = "left";
+      } else if (value > node.value) {
+        parentNode = node;
+        node = node.right;
+        leaf = "right";
+        // Execute once node has been found
+      } else {
+        // If node is a leaf node
+        if (!node.left && !node.right) {
+          parentNode[leaf] = null;
+          return root;
+          // If node has a left and right child
+        } else if (node.left && node.right) {
+          let replacementNode = node.right;
+          parentNode = node;
+          leaf = "right";
+          while (replacementNode.left) {
+            parentNode = replacementNode;
+            replacementNode = replacementNode.left;
+            leaf = "left";
+          }
+          parentNode[leaf] = replacementNode.right;
+          node.value = replacementNode.value;
+          return root;
+          // If node only has one child
+        } else {
+          parentNode[leaf] = node.left ? node.left : node.right;
+          return root;
         }
+      }
+    }
+    // Execute only if node does not exist in tree
+    return root;
+  };
 
-        (callback) ? array.push(callback(node.value)) : array.push(node.value);
-        inOrder(callback, node.left, array);
-        inOrder(callback, node.right, array);
+  const findNode = (value) => {
+    let node = root;
 
-        return array;
+    while (node) {
+      if (value < node.value) {
+        node = node.left;
+      } else if (value > node.value) {
+        node = node.right;
+      } else {
+        return node;
+      }
+    }
+    return null;
+  };
+
+  const levelOrder = (callback) => {
+    if (!root) {
+      return;
     }
 
-    const postOrder = (callback, node = root, array = []) => {
-        if (!node) {
-            return;
-        }
-
-        inOrder(callback, node.left, array);
-        inOrder(callback, node.right, array);
-        (callback) ? array.push(callback(node.value)) : array.push(node.value);
-
-        return array;
+    const array = [];
+    let node;
+    const queue = [root];
+    while (queue.length > 0) {
+      node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      callback ? array.push(callback(node.value)) : array.push(node.value);
     }
 
-    const getHeight = (node = root, height = -1) => {
-        // Return height of -1 if tree is empty
-        if (!node) {
-            return height;
-        }
+    return array;
+  };
 
-        const leftHeight = getHeight(node.left, height + 1);
-        const rightHeight = getHeight(node.right, height + 1);
-
-        return Math.max(leftHeight, rightHeight);
+  const inOrder = (callback, node = root, array = []) => {
+    if (!node) {
+      return;
     }
 
-        const getDepth = (node, rootNode = root, depth = 0) => {
-        // Return height of -1 if tree is empty
-        if (!rootNode || !node) {
-            return -1;
-        }
-        if (node === rootNode) {
-            return depth;
-        }
+    inOrder(callback, node.left, array);
+    callback ? array.push(callback(node.value)) : array.push(node.value);
+    inOrder(callback, node.right, array);
 
-        const leftDepth = getDepth(node, rootNode.left, depth + 1);
-        const rightDepth = getDepth(node, rootNode.right, depth + 1);
+    return array;
+  };
 
-        return Math.max(leftDepth, rightDepth);
+  const preOrder = (callback, node = root, array = []) => {
+    if (!node) {
+      return;
     }
 
-    const isBalanced = (node = root) => {
-        if (!node) {
-            return;
-        }
+    callback ? array.push(callback(node.value)) : array.push(node.value);
+    inOrder(callback, node.left, array);
+    inOrder(callback, node.right, array);
 
-        const leftHeight = getHeight(node.left);
-        const rightHeight = getHeight(node.right);
-        const heightDifference = Math.abs(leftHeight - rightHeight);
-        return (heightDifference < 2);
+    return array;
+  };
+
+  const postOrder = (callback, node = root, array = []) => {
+    if (!node) {
+      return;
     }
 
-    const rebalance = () => {
-        if (!root) {
-            return;
-        }
+    inOrder(callback, node.left, array);
+    inOrder(callback, node.right, array);
+    callback ? array.push(callback(node.value)) : array.push(node.value);
 
-        const array = inOrder();
-        root = _buildTree(array);
-        return root;
+    return array;
+  };
 
+  const getHeight = (node = root, height = -1) => {
+    // Return height of -1 if tree is empty
+    if (!node) {
+      return height;
     }
 
-    return {
-        get root() {
-            return root;
-        },
-        prettyPrint,
-        insertNode,
-        deleteNode,
-        findNode,
-        levelOrder,
-        inOrder,
-        preOrder,
-        postOrder,
-        getHeight,
-        getDepth,
-        isBalanced,
-        rebalance
+    const leftHeight = getHeight(node.left, height + 1);
+    const rightHeight = getHeight(node.right, height + 1);
+
+    return Math.max(leftHeight, rightHeight);
+  };
+
+  const getDepth = (node, rootNode = root, depth = 0) => {
+    // Return height of -1 if tree is empty
+    if (!rootNode || !node) {
+      return -1;
     }
+    if (node === rootNode) {
+      return depth;
+    }
+
+    const leftDepth = getDepth(node, rootNode.left, depth + 1);
+    const rightDepth = getDepth(node, rootNode.right, depth + 1);
+
+    return Math.max(leftDepth, rightDepth);
+  };
+
+  const isBalanced = (node = root) => {
+    if (!node) {
+      return;
+    }
+
+    const leftHeight = getHeight(node.left);
+    const rightHeight = getHeight(node.right);
+    const heightDifference = Math.abs(leftHeight - rightHeight);
+    return heightDifference < 2;
+  };
+
+  const rebalance = () => {
+    if (!root) {
+      return;
+    }
+
+    const array = inOrder();
+    root = _buildTree(array);
+    return root;
+  };
+
+  return {
+    get root() {
+      return root;
+    },
+    prettyPrint,
+    insertNode,
+    deleteNode,
+    findNode,
+    levelOrder,
+    inOrder,
+    preOrder,
+    postOrder,
+    getHeight,
+    getDepth,
+    isBalanced,
+    rebalance,
+  };
 }
 
 module.exports = {
-    TreeNode,
-    Tree
-}
+  TreeNode,
+  Tree,
+};
--- driver.js
+++ driver.js
@@ -1,41 +1,46 @@
-const { TreeNode, Tree } = require("./binary_search_tree");
+const { Tree } = require("./binary_search_tree");
 
 function randomArrayBelowHundred() {
-    // +1 to ensure array has min length 1
-    const array = [];
-    const arrayLength = Math.floor(Math.random() * 20 + 1);
-    for (let i = 0; i < arrayLength; i++) {
-        array.push(Math.floor(Math.random() * 99 + 1));
-    }
-    return array;
+  // +1 to ensure array has min length 1
+  const array = [];
+  const arrayLength = Math.floor(Math.random() * 20 + 1);
+  for (let i = 0; i < arrayLength; i++) {
+    array.push(Math.floor(Math.random() * 99 + 1));
+  }
+  return array;
 }
 
 function randomArrayAboveHundred() {
-    // +1 to ensure array has min length 1
-    const array = [];
-    const arrayLength = Math.floor(Math.random() * 20 + 1);
-    for (let i = 0; i < arrayLength; i++) {
-        array.push(Math.floor(Math.random() * 99 + 100));
-    }
-    return array;
+  // +1 to ensure array has min length 1
+  const array = [];
+  const arrayLength = Math.floor(Math.random() * 20 + 1);
+  for (let i = 0; i < arrayLength; i++) {
+    array.push(Math.floor(Math.random() * 99 + 100));
+  }
+  return array;
 }
 
 let array = randomArrayBelowHundred();
 const tree = Tree(array);
 tree.prettyPrint();
+
 console.log(tree.isBalanced());
+
 console.log(tree.levelOrder());
 console.log(tree.preOrder());
 console.log(tree.inOrder());
 console.log(tree.postOrder());
+
 array = randomArrayAboveHundred();
-for (num of array) {
-    tree.insertNode(num);
+for (const num of array) {
+  tree.insertNode(num);
 }
 tree.prettyPrint();
+
 console.log(tree.isBalanced());
 console.log(tree.rebalance());
 tree.prettyPrint();
+
 console.log(tree.isBalanced());
 console.log(tree.levelOrder());
 console.log(tree.preOrder());
